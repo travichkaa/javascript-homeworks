5f0b07cc9619d11adbb99d99085ae2772e891fe0
Darya Zainullina <babdamino@gmail.com>
2026-09-15 10:37:32 +0500

    Исправлена ошибка в задаче 1

--- 1-booleanTypeAndBranching.js
+++ 1-booleanTypeAndBranching.js
@@ -10,10 +10,10 @@ if(userName === 'admin') {
 } else if(!userName) {
     console.log('Гость');
 } else {
-    if(userBalance > 1000) {
-        console.log('Постоянный покупатель');
-    } else if(userBalance > 5000) {
+    if(userBalance > 5000) {
         console.log('Вип-клиент');
+    } else if(userBalance > 1000) {
+        console.log('Постоянный покупатель');
     }
 }
 
