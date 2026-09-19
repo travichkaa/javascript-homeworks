7c3e50109dc3cbf5a8960969edb18e164aeb68b6
Darya Zainullina <babdamino@gmail.com>
2026-09-19 14:01:30 +0500

    Домашнее задание к занятию "Параметры и возвращаемое значение"

---
+++ 3-parametersAndReturnValue.js
@@ -0,0 +1,162 @@
+// задача 1
+function calculateReputation(countLikes, countDislikes, countEditedMessages, countMessages) {
+    if (countMessages === 0) {
+        return 0;
+    }
+
+    if (countDislikes > countLikes) {
+        return countLikes - countDislikes;
+    }
+
+    return (countLikes - countDislikes) * ((countMessages - countEditedMessages) / countMessages);
+}
+
+let reputation = calculateReputation(10, 15, 10, 20);
+console.log(reputation);
+
+// задача 2
+function printThreadMessages(thread, countMessages) {
+    if (thread.messages.length === 0) {
+        console.log('Ошибка формата! В теме нет сообщений');
+        return;
+    }
+
+    const countMessagesToPrint = Math.min(countMessages, thread.messages.length);
+
+    const messagesToPrint = thread.messages.slice(-countMessagesToPrint);
+
+    for (let message of messagesToPrint) {
+        console.log(`${message.author.name} (репутация: ${message.author.reputation}): ${message.text}`);
+    }
+}
+
+let thread = {
+    title: "Поделитесь книжкой",
+    author: "RuBrick",
+    total: 23,
+    messages: [
+        {
+            id: 13201,
+            date: "2018-01-09",
+            text: "Посоветуйте, пожалуйста, с одной стороны подробную, а с другой доступную для понимания книгу по javascript. Спасибо",
+            author: {
+              name: "RuBrick",
+              login: "ru_brick",
+              reputation: 3,
+              role: "user"
+            }
+        },
+        {
+            id: 13208,
+            date: "2018-01-12",
+            text: "Неужели нет хорошей литературы?",
+            author: {
+              name: "RuBrick",
+              login: "ru_brick",
+              reputation: 3,
+              role: "user"
+            }
+        },
+        {
+            id: 13209,
+            date: "2018-01-12",
+            text: "в общем, NodeJS это JS + некоторые доп. модули и объекты. Тебе нужна литература по самому JS и дока на официальном сайте.",
+            author: {
+              name: "Popov",
+              login: "popov_ma",
+              reputation: 2310,
+              role: "user"
+            }
+        },
+        {
+            id: 13219,
+            date: "2018-01-14",
+            text: "В сети много сайтов с хорошими объяснениями + есть курсы.",
+            author: {
+              name: "Void",
+              login: "void",
+              reputation: 5005,
+              role: "user"
+            }
+        },
+        {
+            id: 13220,
+            date: "2018-01-14",
+            text: "Есть большая книга «JavaScript. Подробное руководство», потом смотришь документацию.",
+            author: {
+              name: "noname",
+              login: "noname",
+              reputation: 100,
+              role: "user"
+            }
+        },
+        {
+            id: 13250,
+            date: "2018-01-19",
+            text: "Или можно посмотреть видеокурсы на YouTube! А самое главное — практика! И этот форум — лучшая тренировочная площадка!",
+            author: {
+              name: "noname",
+              login: "noname",
+              reputation: 110,
+              role: "user"
+            }
+        },
+        {
+            id: 13259,
+            date: "2018-01-20",
+            text: "Понял, спасибо!",
+            author: {
+              name: "RuBrick",
+              login: "ru_brick",
+              reputation: 13,
+              role: "user"
+            }
+        }
+    ]
+};
+
+printThreadMessages(thread, 3);
+
+// задача 3
+function returnStatistics(messages) {
+    const countMessages = messages.length;
+    let countEditedMessages = 0;
+    let countNotEditedMessages = 0;
+    for (let message of messages) {
+        if (message.edited) {
+            countEditedMessages++;
+            continue;
+        }
+        countNotEditedMessages++;
+    }
+
+    return {
+        messages: countMessages,
+        editedMessages: countEditedMessages,
+        notEditedMessages: countNotEditedMessages
+    };
+}
+
+let allMessages = [
+    {author: "zloy-zloy", text: "А у кого какой мобильный??", edited: true},
+    {author: "zloy-zloy", text: "Я с андроидом. Уже 3 года живёт, он самым крепким оказался, пережил 2 утопления.", edited: false},
+    {author: "МамаЗузу", text: "Айфон в своё время успешно сдох при первом же падении на кафельную плитку.", edited: false},
+    {author: "void", text: "У меня андроид. Особенно нравится, что никаких заморочек с айтюнс.", edited: false},
+    {author: "mama", text: "Айфон.", edited: false},
+    {author: "mama", text: "Почему не отвечаешь?", edited: false},
+    {author: "void", text: "Дэвид Хэрман «Сила JavaScript. 68 способов эффективного использования JS».", edited: false},
+    {author: "void", text: "Marijn Haverbeke, Вячеслав Голованов «Выразительный javascript: Введение».", edited: false},
+    {author: "void", text: "Ленюсь.", edited: false},
+    {author: "void", text: "Пока оценивать нечего.", edited: false},
+    {author: "void", text: "Не по-русски как-то получается, хоть и на русском.", edited: false},
+    {author: "ivanov", text: "Чип и Дейл прикольно, играл в детстве.", edited: false},
+    {author: "ivanov", text: "hello, world", edited: true},
+    {author: "void", text: "Сейчас напишу книгу по JS.", edited: false},
+    {author: "ivanov", text: "Спасибо.", edited: false},
+    {author: "ivanov", text: "Смысл такого видео? Все непонятные функции приходится самому смотреть. Надо не так делать. Пишете код — объясняете сразу, что к чему, голосом, ну, или там, текстом хотя бы, хотя лучше голосом.", edited: true},
+    {author: "void", text: "Поделитесь видеоканалами по js.", edited: false},
+    {author: "void", text: "Ничего не понравилось.", edited: false}
+];
+
+let statistics = returnStatistics(allMessages);
+console.log(statistics);
